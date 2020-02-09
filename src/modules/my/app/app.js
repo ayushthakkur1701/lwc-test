790e3615ea5e3016388ced71f3e103ab32b632d7
ayushthakkur1701 <ayushthakur1701@gmail.com>
2020-02-09 16:50:21 +0530

    validation app.js

--- src/modules/my/app/app.js
+++ src/modules/my/app/app.js
@@ -25,17 +25,31 @@ export default class App extends LightningElement {
         //this.showDash = true;
         //this.showLogin = false;
         //this.chlidComponentHeading = 'Dash Board';
+        let mailformat =/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;// /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        let paswd=  /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/;
+        if (!mailformat.test(this.loginEmail)) {
+            this.errorText = 'You have entered an invalid email address!';
+            this.showToast = !this.showToast;
+        } else if (!paswd.test(this.loginPassword)) {
+            this.errorText =
+                'Password must be in between 7 to 15 characters which contain at least one numeric digit and a special character!';
+            this.showToast = !this.showToast;
+        } else {
+            this.showLogin = false;
+            this.chlidComponentHeading = 'Dash Board';
+        }
 
+        /*
         if (
             (this.loginEmail === '' || this.loginEmail == null) &&
             (this.loginPassword === '' || this.loginPassword == null)
         ) {
             this.errorText = 'Please enter Email and Password.';
             this.showToast = !this.showToast;
-            alert(this.showToast);
+            
 
             // validateEmptyCheck(checkEmptyEmail,checkEmptyPassword);
-        } /*else if (
+        } else if (
             (this.loginEmail === '' || this.loginEmail == null) &&
             (this.loginPassword !== '' || this.loginPassword != null)
         ) {
